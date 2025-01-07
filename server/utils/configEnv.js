7bcac93aacaa6824b86ab10578964e969d9244ec
SatyamKumar <kumarsatyam04.2000@gmail.com>
2025-01-07 19:15:21 +0530

    updated

--- server/utils/configEnv.js
+++ server/utils/configEnv.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const environment = process.env.ENVIRONMENT;
+const environment = process.env.NODE_ENV;
 
 // dotenv.config({path: path.join(`${__dirname}`, `../env/dev.env`)});
 
